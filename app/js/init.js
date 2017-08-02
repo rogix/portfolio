aede5863732e7bbe3f84bcea081755c26c8aea52
root <roggeriomarx@gmail.com>
2017-08-01 22:36:33 -0300

    first commit

---
+++ app/js/init.js
@@ -0,0 +1,35 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+    $('.pushpin-nav').each(function() {
+      var $this = $(this);
+      var $target = $('#' + $(this).attr('data-target'));
+      $this.pushpin({
+        top: $target.offset().top,
+        bottom: $target.offset().top + $target.outerHeight() - $this.height()
+      });
+    });
+        
+  }); // end of document ready
+
+  /* Scroll to specific section on front page */
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: (target.offset().top - 80)
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+               
+  
+})(jQuery); // end of jQuery name space
